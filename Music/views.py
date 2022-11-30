ba0b65ea9aacce3f7536f06e8723bd3e16a0e41a
Nathaniel-Mueller <nathaniel.15853.clsec.8@gmail.com>
2022-11-29 21:33:14 -0800

    completely forgot to commit 85% of this... Made Migrations, made serializer, made models, and views, made URLs

---
+++ Music/views.py
@@ -0,0 +1,40 @@
+from django.shortcuts import get_object_or_404
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from rest_framework import status
+from .models import *
+from .serializers import *
+
+@api_view(['GET','POST'])
+def SongList(request):
+    
+    if request.method == 'GET':
+        songs = Song.objects.all()
+        serializer = SongSerializer(songs,many=True)
+        return Response(serializer.data)
+    elif request.method == 'POST':
+        serializer = SongSerializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.data,status=status.HTTP_201_CREATED)
+
+
+@api_view(['GET', 'PUT', 'DELETE'])
+def songDetail(request, pk):
+
+    song = get_object_or_404(Song, pk=pk)
+    if request.method == 'GET':
+        serializer = SongSerializer(song) 
+        return Response(serializer.data,status=status.HTTP_200_OK)
+    elif request.method == 'PUT':
+        serializer = SongSerializer(song, data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.data)
+    elif request.method == 'DELETE':
+        song.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
+        
+
+
+    
